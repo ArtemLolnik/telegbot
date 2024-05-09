482a68f7b3fac4c65ce850085925b1b0086bbf38
Artem <artemch759@gmail.com>
2024-05-09 23:03:47 +0300

    Убрал ссаные сокращения, которые никому не нужны

--- rabochiyibotforIT.py
+++ rabochiyibotforIT.py
@@ -25,46 +25,40 @@ def get_name_surname(message):
     Post = ""
     for i in FrstScndNmNPst.split(" ")[2:]:
         Post += i + " "
-    save_applicant(str(MesFrUsr),str(Imya),str(Familiya))
-    print(f"TGID: {MesFrUsr}")
+    save_applicant(str(message.from_user.id),str(Imya),str(Familiya))
+    print(f"TGID: {message.from_user.id}")
     print(f"Имя: {Imya}")
     print(f"Фамилия: {Familiya}")
     print(f"Должность: {Post}")
-    SendMes(MesFrUsr, "Напишите вашу заявку")
+    SendMes(message.from_user.id, "Напишите вашу заявку")
     RNSH(message, get_query)
 
 # Сообщение-обработчик события
 @T.message_handler(content_types=['text'])
 def get_text_messages(message):
-    global MesFrUsr
-
-    MesFrUsr = message.from_user.id
-    MesText = message.text
 
     # Условия для выполнения разных команд
     # Заявка
-    if MesText == "/query":
-        SendMes(MesFrUsr, "Напишите ваше имя, фамилию, должность\nНапример: Артём Чиженко Специалист технической поддержки")
+    if message.text == "/query":
+        SendMes(message.from_user.id, "Напишите ваше имя, фамилию, должность\nНапример: Артём Чиженко Специалист технической поддержки")
         RNSH(message, get_name_surname)
     # Команда, которая отвечает на заявку
-    elif MesText == "/reply" and MesFrUsr in [662653372, 544333900]:
-        SendMes(MesFrUsr, "Напишите ID для ответа")
+    elif message.text == "/reply" and message.from_user.id in [662653372, 544333900]:
+        SendMes(message.from_user.id, "Напишите ID для ответа")
         RNSH(message, get_reply_query_id)
-    elif MesText == "/quit" and MesFrUsr == 1:
-        T.stop_bot()
     # В любых других случаях
     else:
         KeyboardInline = types.InlineKeyboardMarkup()
         key_query = types.InlineKeyboardButton(text='Отправить заявку', callback_data='/query')
         key_reply = types.InlineKeyboardButton(text='Ответить на заявку', callback_data='/reply')
         KeyboardInline.add(key_query)
-        if MesFrUsr in [662653372, 544333900]:
+        if message.from_user.id in [662653372, 544333900]:
             KeyboardInline.add(key_reply)
-        if MesFrUsr in [662653372, 544333900]:
-            SendMes(MesFrUsr, "/query - отправить заявку в отдел IT\n/reply - ответить на заявку",
+        if message.from_user.id in [662653372, 544333900]:
+            SendMes(message.from_user.id, "/query - отправить заявку в отдел IT\n/reply - ответить на заявку",
                      reply_markup=KeyboardInline)
         else:
-            SendMes(MesFrUsr, "/query - отправить заявку в отдел IT", reply_markup=KeyboardInline)
+            SendMes(message.from_user.id, "/query - отправить заявку в отдел IT", reply_markup=KeyboardInline)
 
 
 
@@ -73,14 +67,12 @@ def get_text_messages(message):
 
 # Отчет
 def get_query(message):
-    global MesText
-    MesText = message.text
-    print(f"Заявка: {MesText}")
+    print(f"Заявка: {message.text}")
     K = types.InlineKeyboardMarkup()
     key_reply_sender = types.InlineKeyboardButton(text='Ответить', callback_data='/reply')
     K.add(key_reply_sender)
-    SendMes(MesFrUsr, "Ваша заявка отправлена!")
-    SendMes(662653372, f"Пользователь '{FrstScndNmNPst}' с ID {MesFrUsr} написал: {MesText}", reply_markup=K)
+    SendMes(message.from_user.id, "Ваша заявка отправлена!")
+    SendMes(662653372, f"Пользователь '{FrstScndNmNPst}' с ID {message.from_user.id} написал: {message.text}", reply_markup=K)
 
 
 # Функции для ответа на заявку
@@ -88,8 +80,7 @@ def get_query(message):
 def get_reply_query_id(message):
     global IDP
     IDP = int(message.text)
-    
-    SendMes(MesFrUsr, "Напишите ваше имя, фамилию")
+    SendMes(message.from_user.id, "Напишите ваше имя, фамилию")
     RNSH(message, get_reply_query_FnLnP)
 
 
@@ -97,22 +88,19 @@ def get_reply_query_id(message):
 def get_reply_query_FnLnP(message):
     global ITFrstScndNmNPst
     ITFrstScndNmNPst = message.text
-    SendMes(MesFrUsr, "Напишите ответ")
+    SendMes(message.from_user.id, "Напишите ответ")
     RNSH(message, get_reply_query)
 
 
 # Ввод ответа и отчет
 def get_reply_query(message):
-    global MesText
-    global IDP
-    MesText = message.text
-    SendMes(MesFrUsr, "Ответ доставлен")
-    get_reply_query_id()
-    SendMes(IDP, f"Пользователь '{ITFrstScndNmNPst}' из отдела IT написал: {MesText}")
+    SendMes(message.from_user.id, "Ответ доставлен")
+    SendMes(IDP, f"Пользователь '{ITFrstScndNmNPst}' из отдела IT написал: {message.text}")
     IDP = 0
 
 
-# Функция-obr otklpoiti
+
+# Функция 
 @T.callback_query_handler(func=lambda call: call.data == '/query' or call.data == '/reply')
 def callback_worker(call: types.CallbackQuery):
     CllFrmUsrId = call.from_user.id
@@ -125,4 +113,4 @@ def callback_worker(call: types.CallbackQuery):
 
 
 
-T.polling(none_stop=True, interval=0.5)
+T.polling(none_stop=True, interval=1)
