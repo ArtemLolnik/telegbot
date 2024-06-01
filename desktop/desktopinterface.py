57b3e9c1c1b717739024e265cfcb8bed4243e34d
Artem <artemch759@gmail.com>
2024-06-01 10:47:29 +0300

    Доработка админ окна

--- desktop/desktopinterface.py
+++ desktop/desktopinterface.py
@@ -1,13 +1,34 @@
 import tkinter as tk
-from tkinter import Label, Button, Entry, Listbox, Text
-from tkinter import scrolledtext
+from tkinter import ttk
+from tkinter import Label, Button, Entry, Listbox
+from connect import get_units_name, save_user_for_button
 
-# Создание главного окна
+def add_user():
+    user_id = entry_id.get()
+    lastname = entry_lastname.get()
+    name = entry_name.get()
+    position = entry_position.get()
+    department = combo_department.get()
+    print(f"""Пользователь:
+user_id: {user_id}
+lastname: {lastname}
+name: {name}
+position: {position}
+department: {department}""")
+    save_user_for_button(user_id,name,lastname)
+
+
+def add_unit():
+    department = combo_department.get()
+    print(f"department: {department}")
+
+
 root = tk.Tk()
-root.title("MainWindow")
+root.title("Административное окно")
 root.geometry("800x450")
 
-# Создание всех элементов управления
+departments = get_units_name()
+
 label_id = Label(root, text="ID TG")
 label_id.place(x=541, y=100, height=25)
 
@@ -35,18 +56,21 @@ entry_name.place(x=587, y=190, width=120, height=25)
 entry_position = Entry(root)
 entry_position.place(x=587, y=280, width=120, height=25)
 
-listbox_department = Listbox(root, height=5)
-listbox_department.place(x=587, y=235, width=125, height=25)
+combo_department = ttk.Combobox(root, height=5, width=20)
+combo_department['values'] = [department.split('(')[0].strip() for department in departments]
+combo_department.current(0)
+combo_department.place(x=587, y=235)
 
-button_add_user = Button(root, text="Добавить пользователя")
+button_add_user = Button(root, text="Добавить пользователя", command=add_user)
 button_add_user.place(x=580, y=345)
 
-button_add_department = Button(root, text="Добавить отдел")
+button_add_department = Button(root, text="Добавить отдел",)
 button_add_department.place(x=255, y=345)
 
-# Создание DataGrid - используем ScrolledText в качестве альтернативы
-data_grid = scrolledtext.ScrolledText(root, width=40, height=10)
-data_grid.place(x=110, y=125)
+listbox_department = Listbox(root, height=5)
+for department in departments:
+    department_name = department.split('(')[0].strip()
+    listbox_department.insert(tk.END, department_name)
+listbox_department.place(x=110, y=125, width=250, height=200)
 
-# Главный цикл обработки событий
 root.mainloop()
